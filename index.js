d22b8b9e22ec541c86cbc6360a7a3e0cca32ec3c
Francesco Cassese <francescocassese9@gmail.com>
2026-06-10 13:23:50 +0200

    aggiunta rotta per testare il server su postman

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from "express";
 
-const App = express();
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-App.use(express.json());
+app.use(express.json());
 
-console.log('Ciao porco');
+app.get('/', (req, res) => {
+    res.json({
+        "message": "Benvenuti"
+    })
+})
 
-App.listen(PORT);
+app.listen(PORT, (error) => {
+    if (error) {
+        return console.log('Il server ha riscontrato un errore', error);
+    }
+    console.log('server in ascolto sulla porta', PORT);
+});
