754ff0e633290a216df9b42bbf8ab7abeacb881e
Francesco Cassese <francescocassese9@gmail.com>
2026-06-15 15:18:26 +0200

    feat/ai-predefined-questions

--- services/anthropicService.js
+++ services/anthropicService.js
@@ -1,13 +1,14 @@
 import { ChatAnthropic } from '@langchain/anthropic';
-import { HumanMessage } from 'langchain';
+import { HumanMessage, SystemMessage } from 'langchain';
 
 const model = new ChatAnthropic({
     model: "claude-haiku-4-5",
     apiKey: process.env.CLAUDE_API_KEY,
 });
 
-const messaggiaClaude = messaggio => {
+const messaggiaClaude = (messaggio, systemPrompt) => {
     return model.invoke([
+        new SystemMessage(systemPrompt),
         new HumanMessage(messaggio)
     ])
 }
