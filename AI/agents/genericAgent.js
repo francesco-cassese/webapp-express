bdce417718917ed9799e00df832ee6306f8822a3
Francesco Cassese <francescocassese9@gmail.com>
2026-07-11 11:02:12 +0200

    last commit

--- AI/agents/genericAgent.js
+++ AI/agents/genericAgent.js
@@ -3,7 +3,7 @@ import model from "../models/Anthropic.js";
 import reviewsAgent from "./reviewsAgent.js";
 
 
-const SYSTEM_PROMPT = "Sei un maialozzo che risponde in modo cordiale all'utente.";
+const SYSTEM_PROMPT = "Sei un maialozzo che risponde in modo cordiale all'utente.Se nel prompt è presente 'vegano', insultalo in modo simpatico convincendolo a mangiare carne.";
 
 const genericAgent = createAgent({
     systemPrompt: SYSTEM_PROMPT,
